3d492952ef0c32a91084c9a2113cf03e01f116e5
casder <casderiopus@gmail.com>
2022-04-18 21:41:08 +0300

    feat: create components for list and movie

---
+++ src/containers/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './../components/card/Card';
+
+class List extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Movie List</h1>
+        <Card />
+      </div>
+    );
+  }
+};
+
+export default List;
