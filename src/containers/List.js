78780ff00ead21be9b0d4ab0e5511d74cffded51
casder <casderiopus@gmail.com>
2022-04-18 22:12:59 +0300

    feat: add functionality for retrieving data

--- src/containers/List.js
+++ src/containers/List.js
@@ -2,6 +2,34 @@ import React from 'react';
 import Card from './../components/card/Card';
 
 class List extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+      isError: false,
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const movies = await fetch('./../assets/data.json');
+      const moviesJson = await movies.json();
+      
+      if (moviesJson) {
+        this.setState({
+          data: moviesJson,
+          loading: false,
+        });
+      }
+    } catch(error) {
+      this.setState({
+        loading: false,
+        isError: true,
+      });
+    }
+  }
+
   render() {
     return (
       <div>
