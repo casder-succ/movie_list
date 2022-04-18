50102ba1969a5c35e8baff39da61e55c527ce76b
casder <casderiopus@gmail.com>
2022-04-18 18:46:46 +0300

    feat: render first react code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello movie list");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Movie list application</h1>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
