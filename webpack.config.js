bfa84580c758d3d9f782a1fe361420b9b540e77d
casder <casderiopus@gmail.com>
2022-04-18 18:46:13 +0300

    feat: configure html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlplugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
 module.exports = {
   module: {
     rules: [
@@ -10,4 +16,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [htmlplugin],
 };
